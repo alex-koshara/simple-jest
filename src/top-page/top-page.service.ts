6c2fe021f14789ff9bdab4fdf7b1dd083781ea03
Koshara Aleksandr <alex.koshara1990@gmail.com>
2024-03-15 21:43:17 +0700

    added text index search and group request

--- src/top-page/top-page.service.ts
+++ src/top-page/top-page.service.ts
@@ -20,11 +20,32 @@ export class TopPageService {
     return this.topPageModel.findOne({ alias }).exec();
   }
 
+  async findByText(text: string) {
+    return this.topPageModel.find({ $text: { $search: text, $caseSensitive: false } }).exec();
+  }
+
   async findByCategory(
     firstCategory: TopLevelCategory,
   ): Promise<DocumentType<TopPageModel, BeAnObject>[]> {
     return this.topPageModel
-      .find({ firstCategory }, { alias: 1, secondCategory: 1, title: 1 })
+      .aggregate([
+        {
+          $match: {
+            firstCategory,
+          },
+        },
+        {
+          $group: {
+            _id: { secondCategory: '$secondCategory' },
+            pages: {
+              $push: {
+                alias: '$alias',
+                title: '$title',
+              },
+            },
+          },
+        },
+      ])
       .exec();
   }
 
