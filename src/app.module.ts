909dfdf442c1805fc0ca3570e0abfcf2adb6d0f0
Koshara Aleksandr <alex.koshara1990@gmail.com>
2024-03-18 14:29:58 +0700

    added sitemap

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { getMongoConfig } from './configs/mongo.config';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { ProductModule } from './product/product.module';
 import { FilesModule } from './files/files.module';
+import { SitemapModule } from './sitemap/sitemap.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { FilesModule } from './files/files.module';
     ProductModule,
     ReviewModule,
     FilesModule,
+    SitemapModule,
   ],
 })
 export class AppModule {}
